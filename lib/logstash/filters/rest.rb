b52907cbf201fbfb1d2c57ff59dccefcc1a22add
Lucas H. <mail@hurb.de>
2015-06-11 16:01:02 +0200

    Update rest.rb

--- lib/logstash/filters/rest.rb
+++ lib/logstash/filters/rest.rb
@@ -12,55 +12,82 @@ class LogStash::Filters::Rest < LogStash::Filters::Base
   # Usage:
   #
   # filter {
-  #   rest {
-  #     url => "http://example.com"
-  #	header => {
-  #		'key1' => 'value1'
-  #		'key2' => 'value2'
-  #		'key3' => '%{somefield}'
-  #	}
-  #	method => "post"
-  #     json => true
-  #	params => {
-  #		'key1' => 'value1'
-  #		'key2' => 'value2'
-  #		'key3' => '%{somefield}'
-  #	}
-  #   }
+  # 	rest {
+  # 		url => "http://example.com"
+  #			method => "post"
+  # 		json => true
+  #			sprintf => true
+  #			params => {
+  #				'key1' => 'value1'
+  #				'key2' => 'value2'
+  #				'key3' => '%{somefield}'
+  #			}
+  # 		header => {
+  #				'key1' => 'value1'
+  #				'key2' => 'value2'
+  #				'key3' => '%{somefield}'
+  #			}
+  #   	}
   # }
   #
   config_name "rest"
   
-  # Replace the message with this value.
   config :url, :validate => :string, :required => true
   config :method, :validate => :string, :default => "get"
   config :json, :validate => :boolean, :default => false
+  config :sprintf, :validate => :boolean, :default => false
   config :header, :validate => :hash, :default => {  }
   config :params, :validate => :hash, :default => {  }
 
   public
   def register
-    @resource = RestClient::Resource.new(@url, :headers => @header)
+
   end # def register
 
   public
   def filter(event)
     return unless filter?(event)
     
-    	begin
-		if method == "get"
-	       		response = @resource.get()
+	begin
+		
+		if sprintf == true
+			begin
+				# sprintf values
+				@url = event.sprintf(@url)
+				@header.each do |key, value|
+					@header[key] = event.sprintf(value)
+				end
+				@params.each do |key, value|
+					@params[key] = event.sprintf(value)
+				end
+			rescue
+				@logger.error("Error during sprintf", :url => url, :method => method, :json => json, :header => header, :params => params)
+				@logger.error("Rest Error Message:", :message => $!.message)
+				@logger.error("Backtrace:", :backtrace => $!.backtrace)
+			end
+		end
+	
+		case method
+		when "get"
+			response = RestClient.get @url, @header
+		when "post"
+			response = RestClient.post @url, @params, @header
 		else
-			response = @resource.post(@params)
+			response = "invalid method"	
+			@logger.error("Invalid method:", :method => method)
 		end
 		
 		if json == true
-		   h = JSON.parse(response)
-		   h.each do |key, value|
-			event[key] = value
-		   end
+			begin
+				h = JSON.parse(response)
+				h.each do |key, value|
+					event[key] = value
+				end
+			rescue
+				event['jsonerror'] = "unable to parse json"
+			end
 		else
-	       	   event['response'] = response.strip
+			event['response'] = response.strip
 		end
 	rescue
 		@logger.error("Error in Rest Filter. Parameters:", :url => url, :method => method, :json => json, :header => header, :params => params)
