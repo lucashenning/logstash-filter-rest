1937d57f3e37e9c3f171280cddbb06d9aca7f6fa
Gandalf Buscher <gandalf.buscher@gmail.com>
2016-07-18 14:56:03 +0200

    fallback testcases

--- spec/filters/rest_spec.rb
+++ spec/filters/rest_spec.rb
@@ -16,6 +16,44 @@ describe LogStash::Filters::Rest do
     sample("message" => "some text") do
       expect(subject).to include("rest_response")
       expect(subject['rest_response']).to include("id")
+      expect(subject['rest_response']).to_not include("fallback")
+    end
+  end
+  describe "Fallback" do
+    let(:config) do <<-CONFIG
+      filter {
+        rest {
+          url => "http://jsonplaceholder.typicode.com/users/0"
+          json => true
+          fallback => {
+            "fallback" => true
+          }
+        }
+      }
+    CONFIG
+    end
+
+    sample("message" => "some text") do
+      expect(subject).to include("rest_response")
+      expect(subject['rest_response']).to include("fallback")
+      expect(subject['rest_response']).to_not include("id")
+    end
+  end
+  describe "Empty response_key" do
+    let(:config) do <<-CONFIG
+      filter {
+        rest {
+          url => "http://jsonplaceholder.typicode.com/users/1"
+          json => true
+          response_key => ""
+        }
+      }
+    CONFIG
+    end
+
+    sample("message" => "some text") do
+      expect(subject).to include("id")
+      expect(subject).to_not include("fallback")
     end
   end
 end
